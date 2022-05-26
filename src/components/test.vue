03e078dc1f6e7cfa67bfc5b61529b02a31a75f3a
liuzz <571794902@qq.com>
2022-05-26 21:49:20 +0800

    测试

---
+++ src/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=''></div>
+</template>
+<script>
+    export default {
+        name: '',
+        props: {},
+        components: {},
+        data() {
+            return {};
+        },
+    };
+</script>
+<style lang='scss' scoped>
+</style>
